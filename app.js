37cc8cdb0ffc17bb11529637e11ecb97180fec42
Akhtar Zaman <auz.zamy@gmail.com>
2018-11-23 23:40:57 -0800

    added handlebars

--- app.js
+++ app.js
@@ -31,7 +31,6 @@ app.get("/school_digger_response", function(req, res){
     res.sendFile(path + "school_digger_response.html")
 })
 
-
 app.listen(9000, function () {
   console.log('Example app listening on port 9000!')
 })
